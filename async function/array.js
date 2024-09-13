507c2bb84dd462251539ceadd47dd6af653d6b90
Agesh k s <71646123+Ageshks@users.noreply.github.com>
2024-09-13 21:21:32 +0530

    Add files via upload

---
+++ async function/array.js
@@ -0,0 +1,12 @@
+let names = ['agesh','ashika','soman','padmavathy'];
+let names2= ['aneesha','arjun'];
+
+//concat
+
+let joined = names.concat(names2)
+console.log(joined);
+
+//splice
+
+names.splice(0,2);
+console.log(names);
---
+++ async function/reduse.js
@@ -0,0 +1,9 @@
+nums = [1,2,3,4,5,6];
+total = nums.reduce((total,currentvalue)=>total+currentvalue);
+console.log(total);
+
+
+function sum(num1,num2){
+    return num1+num2
+}
+sum(10,20);
